8ab79bd92b84f33b8656f3c1e98781c8119a564d
Jack Dwyer <jackndwyer@gmail.com>
2024-09-30 12:20:12 -0700

    Initial Commit

---
+++ GabberExample/GabberExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GabberExampleApp.swift
+//  GabberExample
+//
+//  Created by Jack Dwyer on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct GabberExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
